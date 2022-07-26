f5d51b839aa52fe0a22510ca51cef5f10d77ed1d
songokucoder <miscodices@gmail.com>
2022-07-26 23:08:34 +0200

    tutorial rest api go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/faztweb/go-postgresql-restapi/db"
+	"github.com/faztweb/go-postgresql-restapi/models"
+	"github.com/faztweb/go-postgresql-restapi/routes"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// database connection
+	db.DBConnection()
+	// db.DB.Migrator().DropTable(models.User{})
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
+	r := mux.NewRouter()
+
+	// Index route
+	r.HandleFunc("/", routes.HomeHandler)
+
+	s := r.PathPrefix("/api").Subrouter()
+
+	// tasks routes
+	s.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	s.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	s.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+
+	// users routes
+	s.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	s.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	s.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	s.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	http.ListenAndServe(":4000", r)
+
+}
